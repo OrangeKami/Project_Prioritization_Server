fc8faf81702f63c1791072c795ed0dc45e856240
Chen <97513965+OrangeKami@users.noreply.github.com>
2022-11-04 13:16:19 +1100

    small change
    11

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -1,22 +1,22 @@
-import express from 'express';
+import express from "express";
 import mongoose from "mongoose";
-import Ticket from '../models/ticketModel.js';
-import Ice from '../models/iceModel.js';
+import Ticket from "../models/ticketModel.js";
+import Ice from "../models/iceModel.js";
 
-//  ! get all tickets 
+//  ! get all tickets
 export const getAllTickets = async (req, res) => {
-    try {
-        const  tickets = await Ticket.find().populate('ice');
-        res.status(200).json(tickets)
-    } catch(err) {
-        res.status(404).json({message: err.message})
-    }
-}
+  try {
+    const tickets = await Ticket.find().populate("ice");
+    res.status(200).json(tickets);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
 
 // ! get all submitted tickets
 export const getSubmittedTickets = async (req, res) => {
   try {
-    const submittedTickets = await Ticket.find({isSubmitted: true});
+    const submittedTickets = await Ticket.find({ isSubmitted: true });
     res.status(200).json(submittedTickets);
   } catch (err) {
     res.status(404).json({ message: err.message });
@@ -25,89 +25,88 @@ export const getSubmittedTickets = async (req, res) => {
 
 // ! get single ticket
 export const getSingleTicket = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const ticket = await Ticket.findById(id).populate('ice')
-        res.status(200).json(ticket)
-    } catch(err) {
-        res.status(404).json({message: err.message})
-    }
-}
+  const { id } = req.params;
+  try {
+    const ticket = await Ticket.findById(id).populate("ice");
+    res.status(200).json(ticket);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
 
 // ! create new Ticket
 export const createTicket = async (req, res) => {
-
   console.log(req.body);
   const newIce = new Ice({
     impact: req.body.impact,
     effort: req.body.effort,
-    confidence: req.body.confidence
+    confidence: req.body.confidence,
   });
 
   // * create new Ice
   try {
-    await newIce.save()   
+    await newIce.save();
   } catch (err) {
-    res.status(404).json({message: err.message})
+    res.status(404).json({ message: err.message });
   }
   // * after ice save use its _id as ObjectId to ticket get Ice and Ticket Data nested
   const newTicket = new Ticket({
     initialtive: req.body.initialtive,
     description: req.body.description,
     ice: newIce._id,
-    isSubmitted: req.body.isSubmitted
+    isSubmitted: req.body.isSubmitted,
   });
 
   try {
-    await newTicket.save()
-    res.status(200).json(newTicket)
+    await newTicket.save();
+    res.status(200).json(newTicket);
   } catch (err) {
-    res.status(404).json({message: err.message})
+    res.status(404).json({ message: err.message });
   }
 
-    // const newTicket = new Ticket({
-    //     initialtive: req.body.initialtive,
-    //     description: req.body.description,
-    //     impact: req.body.impact,
-    //     confidence: req.body.confidence,
-    //     effort: req.body.effort, 
-    //     isSubmitted: req.body.isSubmitted,
-    // })
-    // try {
-    //      await newTicket.save();
-    //     res.status(200).json(newTicket);
-    // } catch (err) {
-    //     res.status(500).json({error:err.message});
-    // }
-}
+  // const newTicket = new Ticket({
+  //     initialtive: req.body.initialtive,
+  //     description: req.body.description,
+  //     impact: req.body.impact,
+  //     confidence: req.body.confidence,
+  //     effort: req.body.effort,
+  //     isSubmitted: req.body.isSubmitted,
+  // })
+  // try {
+  //      await newTicket.save();
+  //     res.status(200).json(newTicket);
+  // } catch (err) {
+  //     res.status(500).json({error:err.message});
+  // }
+};
 
 // ! update Ticket
 // ? need to be efficent (Use 2 find method)
 export const updateTicket = async (req, res) => {
-    const {id} = req.params;
-    if (!mongoose.Types.ObjectId.isValid(id))
-      return res.status(404).send(`No post with id: ${id}`);
-      const ticket = await Ticket.findById(id);
-      const updateTicket = {
-      initialtive: req.body.initialtive,
-      description: req.body.description,
-      ice: {
-        impact: req.body.impact,
-        confidence: req.body.confidence,
-        effort: req.body.effort,
-        _id: ticket.ice
-      },
-      _id: id
-    };
-    await Ticket.findByIdAndUpdate(id, updateTicket, {new: true});
-    res.json(updateTicket);
-}
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+  const ticket = await Ticket.findById(id);
+  const updateTicket = {
+    initialtive: req.body.initialtive,
+    description: req.body.description,
+    ice: {
+      impact: req.body.impact,
+      confidence: req.body.confidence,
+      effort: req.body.effort,
+      _id: ticket.ice,
+    },
+    _id: id,
+  };
+  await Ticket.findByIdAndUpdate(id, updateTicket, { new: true });
+  res.json(updateTicket);
+};
 
 // ! delet Ticket
 export const deleteTicket = async (req, res) => {
-    const {id} = req.params;
-    if (!mongoose.Types.ObjectId.isValid(id))
-      return res.status(404).send(`No post with id: ${id}`);
-    await Ticket.findByIdAndRemove(id);
-    res.json({ message:"Ticket deleted successfully"})
-}
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+  await Ticket.findByIdAndRemove(id);
+  res.json({ message: "Ticket deleted successfully" });
+};
--- models/iceModel.js
+++ models/iceModel.js
@@ -1,27 +1,24 @@
- import mongoose, { Schema } from 'mongoose';
- 
- const iceSchema = new Schema({
-   impact: {
-     type: String,
-     required: true,
-     default: "?",
-     enum: ["Small", "Medium", "Large", "Xlarge", "?"],
-     // todo my need a default value
-   },
-   confidence: {
-     type: String,
-     required: true,
-     default: "?",
-     enum: ["Small", "Medium", "Large", "Xlarge", "?"],
-     // todo my need a default value
-   },
-   effort: {
-     type: String,
-     required: true,
-     default: "?",
-     enum: ["Small", "Medium", "Large", "Xlarge", "?"],
-   },
- });
+import mongoose, { Schema } from "mongoose";
 
- export default mongoose.model('Ice', iceSchema);
- 
+const iceSchema = new Schema({
+  impact: {
+    type: String,
+    required: true,
+    default: "?",
+    enum: ["Small", "Medium", "Large", "Xlarge", "?"],
+  },
+  confidence: {
+    type: String,
+    required: true,
+    default: "?",
+    enum: ["Small", "Medium", "Large", "Xlarge", "?"],
+  },
+  effort: {
+    type: String,
+    required: true,
+    default: "?",
+    enum: ["Small", "Medium", "Large", "Xlarge", "?"],
+  },
+});
+
+export default mongoose.model("Ice", iceSchema);
